299d9282d3a0c3c4c0e16ddc924ed9e5a52e1667
Andrew Osheroff <andrewosh@gmail.com>
2016-01-14 17:59:26 -0500

    Revamped startWithPM2. Got logging stack mostly working with docker-compose.

--- services/logging/service.js
+++ services/logging/service.js
@@ -5,11 +5,28 @@ var shell = require('shelljs')
 var format = require('string-format')
 format.extend(String.prototype)
 
-var start = function (opts) {
-  var config = opts.config
+var util = require('../../lib/util.js')
+
+// TODO: move to util file
+var validateConfig = function (config) {
+  if (!config) {
+    return new Error('malformed configuration file: config undefined')
+  }
   if (!config.logging) {
-    console.error('malformed configuration file: no \'logging\' section')
-    process.exit(1)
+    return new Error('malformed configuration file: no \'logging\' section')
+  }
+  return null
+}
+
+/**
+ * Start the logging service (running the ELK stack)
+ * @param {object} config - configuration options
+ * @param {function} cb - cb(err, status)
+ */
+var start = function (config, cb) {
+  var invalid = validateConfig(config)
+  if (invalid) {
+    return cb(invalid)
   }
   var confToVars = {
     'ELASTICSEARCH_PORT': 'logging.elasticsearch.port',
@@ -20,15 +37,22 @@ var start = function (opts) {
   }
   var env = ''
   _.forEach(_.keys(confToVars), function (envVar) {
-    var value = _.get(config, confToVars(envVar))
+    var value = _.get(config, confToVars[envVar])
     if (!value) {
-      console.error('malformed configuration file: {} not specified'.format(envVar))
-      process.exit(1)
+      return cb(new Error('malformed configuration file: {} not specified'.format(envVar)))
     }
-    env += '{0}={1} '.format(envVar, confToVars(envVar))
+    env += '{0}={1} '.format(envVar, _.get(config, confToVars[envVar]))
   })
-  var dcPath = path.join(__dirname, 'services', 'logging', 'docker-compose.yml')
-  shell.exec('{0} docker-compose -f {1} up'.format(env, dcPath))
+  var dcPath = path.join(__dirname, '../../services', 'logging', 'docker-compose.yml')
+  util.startWithPM2({
+    name: 'binder-logging-service',
+    script: '{0} docker-compose'.format(env),
+    args: ['-f', '{0}'.format(dcPath), 'up'],
+    exec_interpreter: 'bash'
+  }, function (err) {
+    console.error('error starting logging service: ' + err.msg)
+    return cb(err)
+  })
 }
 
 module.exports = start
