9c9f40693fc732e4bfeda9b4faf15f1d7d05ce22
Andrew Osheroff <andrewosh@gmail.com>
2016-01-15 18:53:10 -0500

    Almost have full bootstrapping process working (once pm2.start stops hanging)

--- services/db/service.js
+++ services/db/service.js
@@ -1,14 +1,57 @@
+var path = require('path')
 
-var makeImages = function () {
+var _ = require('lodash')
+var shell = require('shelljs')
+var format = require('string-format')
+format.extend(String.prototype)
 
+var util = require('../../lib/util.js')
+
+// TODO: move to util file
+var validateConfig = function (config) {
+  if (!config) {
+    return new Error('malformed configuration file: config undefined')
+  }
+  if (!config.db) {
+    return new Error('malformed configuration file: no \'db\' section')
+  }
+  return null
 }
 
-var makeComposeFile = function () {
-
+/**
+ * Start the database service (running and MongoDB instance)
+ * @param {object} config - configuration options
+ * @param {function} cb - cb(err)
+ */
+var start = function (config, cb) {
+  var invalid = validateConfig(config)
+  if (invalid) {
+    return cb(invalid)
+  }
+  var confToVars = {
+    'MONGODB_PORT': 'db.port',
+    'MONGODB_DIR': 'db.dir'
+  }
+  _.forEach(_.keys(confToVars), function (envVar) {
+    var value = _.get(config, confToVars[envVar])
+    if (!value) {
+      return cb(new Error('malformed configuration file: {} not specified'.format(envVar)))
+    }
+    confToVars[envVar] = value
+  })
+  var dcPath = path.join(__dirname, '../../services', 'db', 'docker-compose.yml')
+  util.startWithPM2({
+    name: 'binder-db-service',
+    env: confToVars,
+    script: shell.which('docker-compose'),
+    args: ['-f', '{0}'.format(dcPath), 'up'],
+    exec_interpreter: 'none'
+  }, function (err) {
+    if (err) {
+      console.error('error starting logging service: ' + err.msg)
+    }
+    return cb(err)
+  })
 }
 
-var bootstrap = function () {
-
-}
-
-module.exports = bootstrap
+module.exports = start
--- services/logging/service.js
+++ services/logging/service.js
@@ -21,7 +21,7 @@ var validateConfig = function (config) {
 /**
  * Start the logging service (running the ELK stack)
  * @param {object} config - configuration options
- * @param {function} cb - cb(err, status)
+ * @param {function} cb - cb(err)
  */
 var start = function (config, cb) {
   var invalid = validateConfig(config)
@@ -35,22 +35,24 @@ var start = function (config, cb) {
     'LOGSTASH_PORT': 'logging.logstash.port',
     'LOGSTASH_CONFIG_DIR': 'logging.logstash.configDir'
   }
-  var env = ''
   _.forEach(_.keys(confToVars), function (envVar) {
     var value = _.get(config, confToVars[envVar])
     if (!value) {
       return cb(new Error('malformed configuration file: {} not specified'.format(envVar)))
     }
-    env += '{0}={1} '.format(envVar, _.get(config, confToVars[envVar]))
+    confToVars[envVar] = value
   })
   var dcPath = path.join(__dirname, '../../services', 'logging', 'docker-compose.yml')
   util.startWithPM2({
     name: 'binder-logging-service',
-    script: '{0} docker-compose'.format(env),
-    args: ['-f', '{0}'.format(dcPath), 'up'],
-    exec_interpreter: 'bash'
+    env: confToVars,
+    script: shell.which('docker-compose'),
+    args: ['-f', dcPath, 'up'],
+    exec_interpreter: 'none'
   }, function (err) {
-    console.error('error starting logging service: ' + err.msg)
+    if (err) {
+      console.error('error starting logging service: ' + err.msg)
+    }
     return cb(err)
   })
 }
